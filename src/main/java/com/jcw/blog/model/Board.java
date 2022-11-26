0c1e6f2da2c158c0c34deb3fc4a224950bfdb61a
JeongChanWoo <ftfgz5093@gmail.com>
2022-11-27 00:08:37 +0900

    Board table reply FetchType.EAGER로 변경

--- src/main/java/com/jcw/blog/model/Board.java
+++ src/main/java/com/jcw/blog/model/Board.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -27,9 +28,13 @@ public class Board {
     @ColumnDefault("0")
     private int count;      //조회수
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "userId")
     private User user;      //DB는 오브젝트를 저장할 수 없음
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) //mappedBy 연관관계의 주인이 아니다 (난 FK가 아님) DB에 칼럼을 만들지 마
+    // EAGER = 한 번에 데이터 가져오기
+    private List<Reply> reply;
 
     @CreationTimestamp      // insert 혹은 update 시간 자동 저장
     private Timestamp createDate;
